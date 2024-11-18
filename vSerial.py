4bd6380f4eaf4af922193b9bebc492b29ae8daa7
gcapan-vescent <gcapan@vescent.com>
2024-11-18 16:31:21 -0700

    Added experimental hardened version of vSerial which is capable of resetting the USB port to recover from comms failures.  Linux only.

--- vSerial.py
+++ vSerial.py
@@ -33,6 +33,9 @@
 ## ========================================================================== ##
 ## 
 
+import subprocess
+from subprocess import DEVNULL
+import os
 import signal
 import serial
 import time
@@ -45,6 +48,7 @@ class vCommHandler():
     def open(self, port):
         try:
             self.ser = serial.Serial(port, baudrate=115200, rtscts=True, dsrdtr=True, xonxoff=True, timeout=0.3, bytesize=8)
+            self.port = port
         except:
             print("Port %s Unavailable" % port)
             return False
@@ -60,31 +64,124 @@ class vCommHandler():
         return
 
     def query(self, command):
+        s = signal.signal(signal.SIGINT, signal.SIG_IGN)
         global debug
         if(debug):
             print("Sending: "+command)
-
-        s = signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-
         command = command + termchars
-        self.ser.write(command.encode())
+        try:
+            self.ser.write(command.encode())
+        except:
+            self.close()
+            success = False
+            loops = 0
+            while not success:
+                loops = loops + 1
+                try:
+                    self.ser.close()
+                    self.ser = serial.Serial(self.port, baudrate=115200, rtscts=True, dsrdtr=True, xonxoff=True, timeout=0.005, bytesize=8)
+                    success = True
+                except Exception as e:
+                    if(debug):
+                        print(e)
+                        print('write command')
+                    success = False
+                    time.sleep(0.1)
+                    if loops >= 10:
+                        if(debug):
+                            print("\n============")
+                            print("TRYING IT!!!")
+                            print("============\n")
+                        #input("press enter when ready.")
+                        subprocess.run("echo '2-1' | sudo tee /sys/bus/usb/drivers/usb/unbind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                        subprocess.run("echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                        time.sleep(0.5)
+                        subprocess.run("echo '2-1' | sudo tee /sys/bus/usb/drivers/usb/bind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                        subprocess.run("echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                        time.sleep(1)
+                    if loops >= 30:
+                        sys.exit(1)
+
+            signal.signal(signal.SIGINT, s)
+            return None
 
         sTime = time.time()
-        while( self.ser.in_waiting == 0 ):
-            if( (time.time() - sTime) > 5 ):
+        in_waiting = 0
+        while( in_waiting == 0 ):
+            try:
+                in_waiting = self.ser.in_waiting
+            except:
+                in_waiting = 0
+
+            if( (time.time() - sTime) > 2 ):
                 self.close()
-                print("No response, connection with device lost.")
-                time.sleep(1)
-                print("The program will now exit.")
-                time.sleep(3)
-                sys.exit(1)
-        retStr = self.ser.readline().decode('ascii')
+                success = False
+                loops = 0
+                while not success:
+                    loops = loops + 1
+                    try:
+                        try:
+                            self.ser.close()
+                        except:
+                            pass
+                        self.ser = serial.Serial(self.port, baudrate=115200, rtscts=True, dsrdtr=True, xonxoff=True, timeout=0.005, bytesize=8)
+                        success = True
+                    except Exception as e:
+                        if debug:
+                            print('waiting '+str(loops)+'...')
+                            print(e)
+                        success = False
+                        time.sleep(0.1)
+                        if loops >= 10:
+                            if debug:
+                                print("\n============")
+                                print("TRYING IT!!!")
+                                print("============\n")
+                                #input("press enter when ready.")
+                            subprocess.run("echo '2-1' | sudo tee /sys/bus/usb/drivers/usb/unbind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                            subprocess.run("echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                            time.sleep(0.5)
+                            subprocess.run("echo '2-1' | sudo tee /sys/bus/usb/drivers/usb/bind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                            subprocess.run("echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                            time.sleep(3)
+                        if loops >= 31:
+                            sys.exit(1)
+                signal.signal(signal.SIGINT, s)
+                return None
+
+        try:
+            retStr = self.ser.readline().decode('ascii')
+        except:
+            self.ser.close()
+            success = False
+            loops = 0
+            while not success:
+                loops = loops + 1
+                try:
+                    self.ser = serial.Serial(self.port, baudrate=115200, rtscts=True, dsrdtr=True, xonxoff=True, timeout=0.005, bytesize=8)
+                    success = True
+                except Exception as e:
+                    if debug:
+                        print('readline')
+                        print(e)
+                    success = False
+                    time.sleep(0.1)
+                    if loops >= 10:
+                        if debug:
+                            print("\n============")
+                            print("TRYING IT!!!")
+                            print("============\n")
+                            #input("press enter when ready.")
+                        subprocess.run("echo '2-1' | sudo tee /sys/bus/usb/drivers/usb/unbind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                        subprocess.run("echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                        time.sleep(0.5)
+                        subprocess.run("echo '2-1' | sudo tee /sys/bus/usb/drivers/usb/bind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                        subprocess.run("echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind",stdout=DEVNULL, stderr=DEVNULL, shell=True)
+                        time.sleep(3)
+
+            retStr = None
 
         signal.signal(signal.SIGINT, s)
-
-        if(debug):
-            print("Response: "+retStr)
 
         return retStr
 
